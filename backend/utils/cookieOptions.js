896326fbe09231a724d2e0204e5853962697fbae
nishuR31 <nishangithub@gmail.com>
2025-08-02 09:47:07 +0530

    cookie options change

--- backend/utils/cookieOptions.js
+++ backend/utils/cookieOptions.js
@@ -1,9 +1,9 @@
 export default function cookieOptions(type = "access") {
   return {
     httpOnly: true,
-    secure: false,
-    maxAge:
-      1000 * 60 * 60 * 24 * (type.toLowerCase().trim() === "access" ? 1 : 7), // 1 day
-    sameSite: "Lax", //"None"
+    secure: true,
+    sameSite: "None",
+    path: "/",  
+    maxAge: 1000 * 60 * 60 * 24 * (type.toLowerCase().trim() === "access" ? 1 : 7), 
   };
 }
