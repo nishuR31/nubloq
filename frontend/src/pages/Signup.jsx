f80d85347de2d2f9d2dc32dd527d0915f9606246
Nishu <nishangithub@gmail.com>
2025-08-08 21:10:14 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,3 +1,215 @@
+// import { Button } from "../components/ui/button";
+// import "../index.css";
+
+// import {
+//   Card,
+//   CardContent,
+//   CardHeader,
+//   CardTitle,
+// } from "../components/ui/card";
+// import { Input } from "../components/ui/input";
+// import { Label } from "../components/ui/label";
+// import React, { useState } from "react";
+// import { Eye, EyeOff } from "lucide-react";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { toast } from "sonner";
+// const api = import.meta.env.VITE_URL;
+
+// const Signup = () => {
+//   const passwordRegex = {
+//     matchLower: {
+//       regex: /(?=.*[a-z])/,
+//       message: "Password must have a lowercase character [a-z].",
+//     },
+//     matchUpper: {
+//       regex: /(?=.*[A-Z])/,
+//       message: "Password must have an uppercase character [A-Z].",
+//     },
+//     matchDigit: {
+//       regex: /(?=.*\d)/,
+//       message: "Password must have a digit [1,2...].",
+//     },
+//     matchSymbol: {
+//       regex: /(?=.*[\W_])/,
+//       message: "Password must have a symbol [!,@...].",
+//     },
+//     matchSpace: {
+//       regex: /^(?!.*\s)/,
+//       message: "Password must not have any spaces between.",
+//     },
+//     matchLength: {
+//       regex: /^.{8,}$/,
+//       message: "Password must be atleast 8 characters long.",
+//     },
+//   };
+//   const passwordErrors = [];
+
+//   const navigate = useNavigate();
+//   const [user, setUser] = useState({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     password: "",
+//     userName: "",
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setUser((prev) => ({
+//       ...prev,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     console.log(user);
+
+//     for (let key in passwordRegex) {
+//       const { regex, message } = passwordRegex[key];
+//       if (!regex.test(user.password)) {
+//         passwordErrors.push(message);
+//       }
+//     }
+
+//     if (passwordErrors.length > 0) {
+//       toast.error(passwordErrors.join("\n"));
+//       passwordErrors.length = 0;
+//       return; // prevent form submission
+//     }
+
+//     try {
+//       // const response = await axios.post(`http://localhost:4000/api/v1/user/register`, user, {
+//       const response = await axios.post(`${api}/user/register`, user, {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         withCredentials: true,
+//       });
+//       console.log(response.data);
+//       if (response.data.success) {
+//         navigate("/login");
+//         toast.success(response.data.message);
+//       } else {
+//         toast.error(response.data.message);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       toast.error(error.response.data.message);
+//     }
+
+//   };
+
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   return (
+//     <div
+//       className={`animate-fadeIn flex h-screen md:pt-14 caret-[var(--primary)] bg-transparent  transition-all delay-[2s] ease-in`}
+//     >
+//       <div className="flex items-center justify-center flex-1 px-4 md:px-0">
+//         <Card className="w-full max-w-md p-6 bg-transparent shadow-lg rounded-2xl backdrop-blur-sm border-sidebar">
+//           <CardHeader>
+//             <CardTitle>
+//               <h1 className="text-xl font-semibold text-app ">
+//                 Create an account
+//               </h1>
+//             </CardTitle>
+//             <p className="mt-2 font-serif text-muted-fg text-sm text-center ">
+//               Enter your details below to create your account
+//             </p>
+//           </CardHeader>
+//           <CardContent>
+//             <form className="space-y-4 " onSubmit={handleSubmit}>
+//               <div className="flex gap-3">
+//                 <div>
+//                   <Label className="text-secondary-fg">First Name</Label>
+//                   <Input
+//                     type="text"
+//                     placeholder="Nishan"
+//                     name="firstName"
+//                     value={user.firstName}
+//                     onChange={handleChange}
+//                     className="placeholder:bg-transparent text-secondary-fg"
+//                   />
+//                 </div>
+
+//                 <div>
+//                   <Label className="text-secondary-fg">Last Name</Label>
+//                   <Input
+//                     type="text"
+//                     placeholder=" "
+//                     name="lastName"
+//                     value={user.lastName}
+//                     onChange={handleChange}
+//                     className="placeholder:bg-transparent text-secondary-fg"
+//                   />
+//                 </div>
+//               </div>
+//               <div>
+//                 <Label className="text-secondary-fg">Username</Label>
+//                 <Input
+//                   type="text"
+//                   placeholder="username_"
+//                   name="userName"
+//                   value={user.userName}
+//                   onChange={handleChange}
+//                   className="placeholder:bg-transparent text-secondary-fg"
+//                 />
+//               </div>
+//               <div>
+//                 <Label className="text-secondary-fg">Email</Label>
+//                 <Input
+//                   type="email"
+//                   placeholder="abc123@mail.com"
+//                   name="email"
+//                   value={user.email}
+//                   onChange={handleChange}
+//                   className="placeholder:bg-transparent text-secondary-fg"
+//                 />
+//               </div>
+
+//               <div className="relative">
+//                 <Label className="text-secondary-fg">Password</Label>
+//                 <Input
+//                   type={showPassword ? "text" : "password"}
+//                   placeholder="Create a Password"
+//                   name="password"
+//                   value={user.password}
+//                   onChange={handleChange}
+//                   className="placeholder:bg-transparent text-secondary-fg"
+//                 />
+//                 <button
+//                   type="button"
+//                   className="absolute text-secondary-fg right-3 top-8"
+//                   onClick={() => setShowPassword(!showPassword)}
+//                 >
+//                   {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+//                 </button>
+//               </div>
+
+//               <Button type="submit" className="w-full bg-primary text-secondary-fg">
+//                 Sign Up
+//               </Button>
+//               <p className="text-center text-muted-fg">
+//                 Already have an account?{" "}
+//                 <Link to={"/login"}>
+//                   <span className="hover:underline cursor-pointer text-secondary-fg ">
+//                     Sign in
+//                   </span>
+//                 </Link>
+//               </p>
+//             </form>
+//           </CardContent>
+//         </Card>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Signup;
+
+// //////////////////////////////////////////////
 import { Button } from "../components/ui/button";
 import "../index.css";
 
@@ -14,6 +226,8 @@ import { Eye, EyeOff } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "sonner";
+  import { useLocation } from "react-router-dom";
+
 const api = import.meta.env.VITE_URL;
 
 const Signup = () => {
@@ -46,6 +260,9 @@ const Signup = () => {
   const passwordErrors = [];
 
   const navigate = useNavigate();
+const location = useLocation();
+const redirect = new URLSearchParams(location.search).get("redirect") || "/";
+
   const [user, setUser] = useState({
     firstName: "",
     lastName: "",
@@ -64,7 +281,6 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(user);
 
     for (let key in passwordRegex) {
       const { regex, message } = passwordRegex[key];
@@ -87,9 +303,8 @@ const Signup = () => {
         },
         withCredentials: true,
       });
-      console.log(response.data);
       if (response.data.success) {
-        navigate("/login");
+        navigate(`/login?redirect=${redirect}`);
         toast.success(response.data.message);
       } else {
         toast.error(response.data.message);
@@ -193,7 +408,7 @@ const Signup = () => {
               </Button>
               <p className="text-center text-muted-fg">
                 Already have an account?{" "}
-                <Link to={"/login"}>
+              <Link to={`/login?redirect=${redirect}`}>
                   <span className="hover:underline cursor-pointer text-secondary-fg ">
                     Sign in
                   </span>
