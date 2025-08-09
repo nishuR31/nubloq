97e7910a14ec19ee0cc258015bccaea63655e187
Nishu <nishangithub@gmail.com>
2025-08-09 21:49:01 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -405,7 +405,7 @@ export const logout = asyncHandler(async (req, res) => {
     .status(codes.ok)
     .json(
       new ApiResponse(
-        `${req.user?.userName??"Your are"} successfully logged out.`,
+        `You are successfully logged out.`,
         codes.ok
       ).res()
     );
