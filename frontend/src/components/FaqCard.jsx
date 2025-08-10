33117c970ec53c16abfe0b951e29f951fbc3b3bc
Nishu <nishangithub@gmail.com>
2025-08-11 04:10:38 +0530

    Update FaqCard.jsx

--- frontend/src/components/FaqCard.jsx
+++ frontend/src/components/FaqCard.jsx
@@ -12,16 +12,16 @@ export default function FaqCard({ question, answer }) {
 
   return (
     <div
-      className={`transition-all delay-3000 ease-in animate-slideInLeft p-4 mb-4 rounded-2xl shadow-md backdrop-blur-sm bg-transparent`}
+      className={`transition-all delay-3000 ease-in p-4 mb-4 rounded-2xl shadow-md backdrop-blur-sm bg-transparent`}
     >
       <Button
       variant="ghost"
-        className="w-full text-lg font-semibold text-center text-gray-800 dark:text-white "
+        className="w-full text-lg font-semibold text-center text-secondary-fg "
         onClick={() => setOpen(!open)}
       >
         {question}
       </Button>
-      {open && <p className="mt-2 text-sm text-gray-800 dark:text-gray-300">{answer}</p>}
+      {open && <p className="mt-2 text-sm text-secondary-fg">{answer}</p>}
     </div>
   );
 }
